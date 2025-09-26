b0f5656b4f3f678bf2feba639cf825034944ba0a
ellylai <ellysel@andrew.cmu.edu>
2025-09-26 17:11:08 -0400

    pipeline

---
+++ code/parsing_pipeline.py
@@ -0,0 +1,5 @@
+# 1. scraper: use links in google doc to scrape for all subpages and pdfs
+# parent page and subpage links go in html_links.json
+# pdfs go in a folder in pdfs_and_html_links
+# 2. html_parser: use links in html_links.json to convert to plain text (put in a folder)
+# 3. pdf_parser: use downloaded pdf files to convert to plain text (put in a folder)
