310f6707161b05813641d4b487a4e22f219bdf1b
ellylai <ellysel@andrew.cmu.edu>
2025-10-01 11:45:19 -0400

    sparse retriever

--- code/retrievers/sparse.py
+++ code/retrievers/sparse.py
@@ -1,18 +1,28 @@
-'''
-Sparse Retrieval: Integrate BM25 into your pipeline. You can use existing BM25 implementations, 
+"""
+Sparse Retrieval: Integrate BM25 into your pipeline. You can use existing BM25 implementations,
 limited to bm25s or rank-bm25, or build your own implementation if you wish to do so.
 
-Citation: used https://github.com/xhluca/bm25s 
-'''
+Citation: used https://github.com/xhluca/bm25s
+"""
 
 import bm25s
 
-def sparse_retriever(corpus):
-    corpus_tokens = bm25s.tokenize(corpus, stopwords = "en")
-    
+# INCOMPLETE
+
+
+def sparse_retriever_corpus(corpus):
+    corpus_tokens = bm25s.tokenize(corpus, stopwords="en")
     retriever = bm25s.BM25()
     retriever.index(corpus_tokens)
-    
     retriever.save("retriever")
-    
-    
+
+
+def sparse_retriever(query):
+    query_tokens = bm25s.tokenize(query)
+
+    retriever = bm25s.BM25.load("retriever", load_corpus=True)
+
+    results, scores = retriever.retrieve(query_tokens, k=2)
+    for i in range(results.shape[1]):
+        doc, score = results[0, i], scores[0, i]
+        print(f"Rank {i+1} (score: {score:.2f}): {doc}")
