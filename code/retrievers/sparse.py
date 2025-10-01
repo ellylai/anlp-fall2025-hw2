f92ed4a5c941fd9c292e18ae4154f5ef575d80a8
ellylai <ellysel@andrew.cmu.edu>
2025-10-01 11:41:38 -0400

    sparse retriever

--- code/retrievers/sparse.py
+++ code/retrievers/sparse.py
@@ -1,7 +1,18 @@
 '''
 Sparse Retrieval: Integrate BM25 into your pipeline. You can use existing BM25 implementations, 
 limited to bm25s or rank-bm25, or build your own implementation if you wish to do so.
+
+Citation: used https://github.com/xhluca/bm25s 
 '''
 
-def sparse_retriever():
-    pass
+import bm25s
+
+def sparse_retriever(corpus):
+    corpus_tokens = bm25s.tokenize(corpus, stopwords = "en")
+    
+    retriever = bm25s.BM25()
+    retriever.index(corpus_tokens)
+    
+    retriever.save("retriever")
+    
+    
