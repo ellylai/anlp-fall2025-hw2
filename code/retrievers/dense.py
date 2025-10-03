afcf2b3cc9b2c163be11a246275692fbeed8d90c
Sophia Judicke <sophiajudicke@sophias-mbp-3.wifi.local.cmu.edu>
2025-10-02 20:10:40 -0400

    Adding skeleton code dense retriever

--- code/retrievers/dense.py
+++ code/retrievers/dense.py
@@ -8,20 +8,28 @@ You'll need to implement the pipeline for embedding documents, building the FAIS
 retrieving similar documents for queries.
 '''
 from sentence_transformers import SentenceTransformer
+import faiss
 
 def embedding(encoder, docs):
     return encoder.encode(docs, convert_to_numpy=True)
 
-def faiss(q_emb, embs):
-    pass
-
-def build_database(docs):
+def process_data(docs):
     encoder = SentenceTransformer("all-MiniLM-L6-v2")
     embeddings = embedding(encoder, docs)
     idxs = faiss(embeddings)
     return idxs, encoder
 
+def build_database(embs):
+    d = embs[0].shape
+    database = faiss.IndexFlatL2(d)
+    database.add(embs)
+    return database
+
+def search_data(q_emb, data, k):
+    docs = data.search(q_emb, k)
+    return docs
+
 def dense_retriever(query, data, encoder):
     q_emb = embedding(encoder, [query])
-    docs = faiss(q_emb, data)
+    docs = search_data(q_emb, data, k=60)
     return docs
